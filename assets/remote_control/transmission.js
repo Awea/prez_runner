f76fa0c0c087695f2c63fc8412f815c41d5871ee
Awea <aweaoftheworld@gmail.com>
2014-01-13 00:09:51 +0100

    gameboy with endpoints for addons

--- assets/remote_control/transmission.js
+++ assets/remote_control/transmission.js
@@ -1,32 +1,43 @@
 $(function() {
   var conn;
-  var msg       = $("#msg");
-  var log       = $("#log");
-  var ws_server = $("body").data('ip');
-
-  function appendLog(msg) {
-    var d = log[0]
-    var doScroll = d.scrollTop == d.scrollHeight - d.clientHeight;
-    msg.appendTo(log)
-    if (doScroll) {
-      d.scrollTop = d.scrollHeight - d.clientHeight;
-    }
-  }
-
-  $(".controls div").click(function(evt) {
-    evt.preventDefault();
-    conn.send($(this).data('command'));
-  });
+  var title        = 'Remote control - ';
+  var msg          = $("#msg");
+  var log          = $("#log");
+  var ws_server    = $("body").data('ip');
+  var modes        = ['prez', 'window', 'tab'];
+  var current_mode;
 
   if (window["WebSocket"]) {
     conn = new WebSocket("ws://" + ws_server + ":8080/ws");
-    conn.onclose = function(evt) {
-      appendLog($("<div><b>Connection closed.</b></div>"))
-    }
-    conn.onmessage = function(evt) {
-      appendLog($("<div/>").text(evt.data))
-    }
   } else {
     appendLog($("<div><b>Your browser does not support WebSockets.</b></div>"))
   }
+
+  conn.onopen = function(){
+    set_current_mode();
+  }
+
+  function set_current_mode(mode_id){
+    mode_id = typeof mode_id !== 'undefined' ? mode_id : 0;
+
+    current_mode = modes[mode_id];
+    document.title = title + current_mode;
+    conn.send(current_mode);
+  }
+
+  function next_mode(){
+    current_mode_index = modes.indexOf(current_mode);
+    if (current_mode_index + 1 < modes.length)
+      set_current_mode(current_mode_index + 1)
+    else
+      set_current_mode(0)
+  }
+
+  $(".btn").click(function(evt) {
+    conn.send($(this).data('command'));
+  });
+
+  $("#btn-select").click(function(evt){
+    next_mode();
+  });
 });
